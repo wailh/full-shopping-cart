16dc75cb69aebabc3dd456e54c7f3e4df57ca270
wail hamdi <hamdi.djiha02@gmail.com>
2022-02-27 00:56:19 +0100

    feature 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+// feature-1
 import "./App.css";
 
 const App = () => {
-
+    return (
+        <div className='grid-container'>
+            <header>
+                <a href="#">React Shopping Cart</a>
+            </header>
+            <main>
+                 hello main
+            </main>
+            <footer>
+                All rights reserved
+            </footer>
+        </div>
+    )
 }
 
 export default App;
